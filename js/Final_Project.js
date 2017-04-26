8153ef9f5985fa6a12eea0e01dcf60858a595c61
ErichOCallaghan <S01408883@FRCCinst.ccofc.edu>
2017-04-25 18:44:41 -0600

    Add Widget and Layer JS
    Added basemap toggle widget and RMNP boundary layer.

--- js/Final_Project.js
+++ js/Final_Project.js
@@ -1,9 +1,10 @@
 require([
       "esri/Map",
       "esri/views/SceneView",
+      "esri/widgets/BasemapToggle",
       "esri/layers/FeatureLayer",
       "dojo/domReady!"
-    ], function (Map, SceneView, FeatureLayer) {
+    ], function (Map, SceneView, BasemapToggle, FeatureLayer) {
 
     var map = new Map({
         basemap: "hybrid"
@@ -15,9 +16,23 @@ require([
         center: [-101.17, 21, 78],
         scale: 50000000
     });
+
+    var basemapToggle = new BasemapToggle({
+        view: view, // The view that provides access to the map's "streets" basemap
+        nextBasemap: "topo" // Allows for toggling to the "hybrid" basemap
+    });
+
+    view.ui.add(basemapToggle, "top-right");
+
+
     var campsites = new FeatureLayer({
         url: "https://services1.arcgis.com/fBc8EJBxQRMcHlei/arcgis/rest/services/ROMO_BackcountryCampsites_2017/FeatureServer/0"
     });
     map.add(campsites);
+    
+    var RMNPboundary = new FeatureLayer({
+        url: "https://services1.arcgis.com/fBc8EJBxQRMcHlei/ArcGIS/rest/services/RockyMountainNP_Boundary/FeatureServer/0"
+    });
+    map.add(RMNPboundary);
 
 });
