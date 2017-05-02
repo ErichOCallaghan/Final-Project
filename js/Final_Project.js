232e6d34c34ac79de84a7391114251ef99cc7c4d
ErichOCallaghan <eric.ocallaghan@digitalglobe.com>
2017-05-02 12:48:04 -0600

    Broken Locator Popup
    Added code to add a location coordinate popup as an onclick event.  It
    did not work, but shows what I tried.

--- js/Final_Project.js
+++ js/Final_Project.js
@@ -3,8 +3,9 @@ require([
       "esri/views/SceneView",
       "esri/widgets/BasemapToggle",
       "esri/layers/FeatureLayer",
+      "esri/tasks/Locator",
       "dojo/domReady!"
-    ], function (Map, SceneView, BasemapToggle, FeatureLayer) {
+    ], function (Map, SceneView, BasemapToggle, FeatureLayer, Locator) {
 
     var map = new Map({
         basemap: "hybrid"
@@ -29,15 +30,45 @@ require([
         url: "https://services1.arcgis.com/fBc8EJBxQRMcHlei/arcgis/rest/services/ROMO_BackcountryCampsites_2017/FeatureServer/0"
     });
     map.add(campsites);
-    
+
     var RMNPboundary = new FeatureLayer({
         url: "https://services1.arcgis.com/fBc8EJBxQRMcHlei/ArcGIS/rest/services/RockyMountainNP_Boundary/FeatureServer/0"
     });
     map.add(RMNPboundary);
-    
+
     var CubLakeTrail = new FeatureLayer({
         url: "http://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/CubLakeTrail/FeatureServer"
     });
     map.add(CubLakeTrail);
+
+    var locatorTask = new Locator({
+        url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
+    });
+
+    view.on("click", function (event) {
+        // Get the coordinates of the click on the view
+        var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
+        var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
+
+        view.popup.open({
+            // Set the popup's title to the coordinates of the location
+            title: "Reverse geocode: [" + lon + ", " + lat + "]",
+            location: event.mapPoint // Set the location of the popup to the clicked location
+        });
+
+        // Display the popup
+        // Execute a reverse geocode using the clicked location
+        locatorTask.locationToAddress(event.mapPoint).then(function (
+            response) {
+            // If an address is successfully found, print it to the popup's content
+            var address = response.address.Match_addr;
+            view.popup.content = address;
+        }).otherwise(function (err) {
+            // If the promise fails and no result is found, print a generic message
+            // to the popup's content
+            view.popup.content =
+                "No address was found for this location";
+        });
+    });
 
 });
